299e1b77ee8816e3409552d46d8ca17aeadb5744
Juan Carlos <juancho131401@gmail.com>
2020-05-05 15:56:38 -0500

    Amikos
    Ultima prueba hosting-git

--- lynxgame/src/App.js
+++ lynxgame/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ya vamo a trabajar
+          Ya vamo a trabajar, amikos
         </p>
         <a
           className="App-link"
