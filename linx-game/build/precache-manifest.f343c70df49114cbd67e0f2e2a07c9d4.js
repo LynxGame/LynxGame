d214156f02e98454774466af85ffc51998191da7
Juan Carlos <juancho131401@gmail.com>
2020-05-25 17:39:17 -0500

    Build
    jqasdfasdfasdfas

---
+++ linx-game/build/precache-manifest.f343c70df49114cbd67e0f2e2a07c9d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "282278104a4ee01679d8f0ff87443122",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3276504a5d5959aee72",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "678d460fe06e483a8090",
+    "url": "/static/js/2.365510fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.365510fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3276504a5d5959aee72",
+    "url": "/static/js/main.11881259.chunk.js"
+  },
+  {
+    "revision": "f7d1b54310d8c7d9c79e",
+    "url": "/static/js/runtime-main.7680b03d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
