f7179cc807f35c064fa5d7c854ec9d45956097c7
Carlos Catarino <carlos_cata27@hotmail.com>
2020-05-26 17:06:25 -0500

    Revert "IconoDrawer"
    This reverts commit 7483f6d1e571efb2d8d565dee8c232c9b65dca0e.

--- linx-game/src/Drawer.js
+++ linx-game/src/Drawer.js
@@ -3,7 +3,6 @@ import {  Avatar, Button } from 'antd'
 import {   FacebookFilled , YoutubeFilled, TwitterCircleFilled, InstagramFilled  ,GithubFilled} from '@ant-design/icons';
 import { createFromIconfontCN } from '@ant-design/icons';
 import { Row, Col } from 'antd';
-import icono from './img/icono.svg';
 
 const IconFont = createFromIconfontCN({scriptUrl:  '//at.alicdn.com/t/font_1842114_fpuw4z74sx.js'});
 
@@ -13,9 +12,7 @@ export class Drawer extends Component {
             <div>
             <Row gutter={[24, 24]}>
                 <Col>
-                    <Avatar style={{ 
-                        background: '#none'
-                    }}src='https://raw.githubusercontent.com/LynxGame/LynxGame/master/linx-game/src/img/icono.png?token=AMFFPBXF4MY7I332GV2O4QK6ZWINM' size={190}/>
+                    <Avatar style={{ }} size={190} src='./img/dummy.png'/>
                 </Col>
             </Row>
             
@@ -30,7 +27,7 @@ export class Drawer extends Component {
                     <Button type="link" icon={<YoutubeFilled style={{fontSize: '40px'}}/>} href="https://www.youtube.com"/>
                 </Col>
                 <Col span={12} key="Facebook">
-                <Button type="link" icon={<FacebookFilled style={{fontSize: '40px'}}/>} href="https://www.facebook.com/LynxxT"/>
+                <Button type="link" icon={<FacebookFilled style={{fontSize: '40px'}}/>} href="https://www.facebook.com/CarlosCata27"/>
                 </Col>
             </Row>
 
