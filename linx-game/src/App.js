24d8d3de43d6e03ec42437cec6607667dd2bf1aa
Carlos Catarino <carlos_cata27@hotmail.com>
2020-05-25 19:48:32 -0500

    Footer,Header,Sider
    Ahora son estaticos

--- linx-game/src/App.js
+++ linx-game/src/App.js
@@ -8,16 +8,16 @@ const { Header, Footer, Sider, Content } = Layout;
 function App() {
   return (
     <Layout>
-      <Header style={{}}><Toolbar/></Header>
+      <Header style={{position: "sticky", top: "0"}}><Toolbar/></Header>
       <Layout>
-        <Sider style={{background: "white", overflow: 'auto', left:0}}>Lynx Game</Sider>
+        <Sider style={{background: "white", overflow: 'auto', left:0,theme: "light",position: "static"}}>Lynx Game</Sider>
         <Content>
           <div style={{  height: "calc(100vh - 55px)" }}>
             A lot of games to me c:
           </div>
       </Content>
       </Layout>
-      <Footer>Todos los derechos reservados Ant Design ©2018 Created by Ant UED</Footer>
+      <Footer style={{ position: "sticky", bottom: "0" }}>Todos los derechos reservados Ant Design ©2018 Created by Ant UED</Footer>
     </Layout>
   );
 }
