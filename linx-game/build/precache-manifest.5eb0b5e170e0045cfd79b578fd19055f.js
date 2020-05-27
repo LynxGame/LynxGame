a64fde1041ae7b5ba07d6bdbc1b0d91b332a1ac5
Carlos Catarino <carlos_cata27@hotmail.com>
2020-05-27 17:43:22 -0500

    Firebase

---
+++ linx-game/build/precache-manifest.5eb0b5e170e0045cfd79b578fd19055f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7888fd12668e9fcf74ff28e1171cc74b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f3dc8c2add3b24f5190",
+    "url": "/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "122aee0316ad61943ae5",
+    "url": "/static/css/main.1b72be62.chunk.css"
+  },
+  {
+    "revision": "0f3dc8c2add3b24f5190",
+    "url": "/static/js/2.9812b26d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9812b26d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "122aee0316ad61943ae5",
+    "url": "/static/js/main.8e0ca7a5.chunk.js"
+  },
+  {
+    "revision": "f7d1b54310d8c7d9c79e",
+    "url": "/static/js/runtime-main.7680b03d.js"
+  }
+]);
