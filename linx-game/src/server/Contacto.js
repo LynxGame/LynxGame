55215673ad409cfa509b467fd35caace70ffccd3
Helix320 <64866893+Helix320@users.noreply.github.com>
2020-05-27 13:51:38 -0500

    Update Contacto.js

--- linx-game/src/server/Contacto.js
+++ linx-game/src/server/Contacto.js
@@ -4,7 +4,7 @@ class Contacto {
     constructor(oConfig){
         this.oConfig=oConfig;
     }
-    agregarUsuario(Nombre,Apellido,Correo,Contraseña,Username){
+    agregarUsuario(Nombre,Apellido,Username,Correo,Edad,Contraseña){
         var con = mysql.createConnection(this.oConfig);
         con.connect(function(error){
             try {
@@ -13,19 +13,24 @@ class Contacto {
                 }
                 else{
                     console.log("Conexion exitosa");
-                    con.query("SELECT COUNT(*) AS USUARIOS FROM USUARIO",function(error,res,campo){
+                    con.query("SELECT COUNT(*) AS USUARIOS FROM clientes_lynxgame",function(error,res,campo){
                         if(error){
                             console.log("Error en select de la DB --" + console.error());
                         }
                         else{
                             console.log("Usuarios encontrados: " + res[0].usuario);
                             if(parseInt(res[0].usuario)==0){
-                                con.query('INSET INTO usuarios (Nombre,Apellidos,Correo,Contrasenia,Username)',function(error,res,campo){
-                                    
-                                })
+                                con.query('INSET INTO clientes_lynxgame (Nombre,Apellidos,Correo,Contrasenia,Username) values('${Nombre}','${Apellido}','${Username}','${Correo}','${Edad}','${Contraseña}')',function(error,res,campo){
+                                    if(error){
+                                        console.log("Error al insertar nuevo usuario en la DB --" + console.error());
+                                    }
+                                    else{
+                                        console.log("Nuevo usuario registrado");
+                                    }
+                                });
                             }
                         }
-                    })
+                    });
                 }
 
             } catch (x) {
