d480acb077210257195149b216623e842cf3e699
Juan Carlos <juancho131401@gmail.com>
2020-05-26 23:56:52 -0500

    Update Build

--- linx-game/build/precache-manifest.08b0314b715a0e0bd62c216febda2d5b.js
+++ linx-game/build/precache-manifest.08b0314b715a0e0bd62c216febda2d5b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e35a3f944d69da98219f01d89f35dbd9",
+    "revision": "bb4653481637fce07428b26e9d91a338",
     "url": "/index.html"
   },
   {
-    "revision": "24a8c24ee432fa695819",
+    "revision": "cca4c129398361e43f23",
     "url": "/static/css/2.63b65d4e.chunk.css"
   },
   {
-    "revision": "739b7362b515a8e517a6",
+    "revision": "5e9cc3d56420eb05a90f",
     "url": "/static/css/main.e83f2f0f.chunk.css"
   },
   {
-    "revision": "24a8c24ee432fa695819",
-    "url": "/static/js/2.e3fa8d6c.chunk.js"
+    "revision": "cca4c129398361e43f23",
+    "url": "/static/js/2.a3a7ba34.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.e3fa8d6c.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.a3a7ba34.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "739b7362b515a8e517a6",
-    "url": "/static/js/main.1ef021c5.chunk.js"
+    "revision": "5e9cc3d56420eb05a90f",
+    "url": "/static/js/main.d2225ac7.chunk.js"
   },
   {
     "revision": "f7d1b54310d8c7d9c79e",
