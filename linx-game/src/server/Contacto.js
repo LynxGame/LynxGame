22c8281cf6b66a51ea91a72405c69903ed6ac3d5
Carlos Catarino <carlos_cata27@hotmail.com>
2020-05-27 13:24:08 -0500

    Paqueteria MySQL
    Paqueteria agregada

---
+++ linx-game/src/server/Contacto.js
@@ -0,0 +1,35 @@
+const mysql = require("mysql");
+
+class Contacto {
+    constructor(oConfig){
+        this.oConfig=oConfig;
+    }
+    agregarUsuario(Nombre,Apellido,Correo,Contraseña,Username){
+        var con = mysql.createConnection(this.oConfig);
+        con.connect(function(error){
+            try {
+                if(error){
+                    console.log("Error al establecer la conexion a la base de datos --" + error);
+                }
+                else{
+                    console.log("Conexion exitosa");
+                    con.query("SELECT COUNT(*) AS USUARIOS FROM USUARIO",function(error,res,campo){
+                        if(error){
+                            console.log("Error en select de la DB --" + console.error());
+                        }
+                        else{
+                            console.log("Usuarios encontrados: " + res[0].usuario);
+                            if(parseInt(res[0].usuario)==0){
+                                
+                            }
+                        }
+                    })
+                }
+
+            } catch (x) {
+                
+            }
+        })
+    }
+}
+module.exports = Contacto;
