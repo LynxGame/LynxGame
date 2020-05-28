5dd9eb430c2d5c03f070a0ea94d8b7176f57d25e
Carlos Catarino <carlos_cata27@hotmail.com>
2020-05-27 20:25:03 -0500

    Vista general
    Vista general para al abrir videojuegos

--- linx-game/src/Vista/ViewGame.js
+++ linx-game/src/Vista/ViewGame.js
@@ -6,27 +6,59 @@ export class ViewGame extends Component {
     render() {
     return (
         <div>
-          <Row>
+          <Row gutter={16}>
+
+            <h1>Nombre del juego</h1>
+
+          </Row>
+          <Row gutter={16}>
             
-          Aqui va el carrusel juas juas
+          <h1>Aqui va el carrusel juas juas</h1>
 
           </Row>
 
-          <Row>
+          <Row gutter={16}>
 
-            <Col>
-              Descripcion del game
+            <Col span={12}>
+              <h1>Descripcion del game</h1>
             </Col>
 
-            <Col>
+            <Col span={12}>
+            <Row gutter={16}>
+              <h1>Precio</h1>
+            </Row>
+            <Row gutter={16}>
             <Button
             type="primary"
             htmlType="submit"
-          >
+            >
             Jugar
           </Button>
+            </Row>
+            
             </Col>
 
+          </Row>
+
+          <Row gutter={16}>
+
+            <Row gutter={16}>
+              <Col span={12}>
+                <h1>Genero</h1>
+              </Col>
+              <Col span={12}>
+              <h1>Desarrollador</h1>
+              </Col>
+            </Row>
+
+            <Row gutter={16}>
+              <Col span={12}>
+              <h1>Fecha de lanzamiento</h1>
+              </Col>
+              <Col span={12}>
+              <h1>Edad</h1>
+              </Col>
+            </Row>
           </Row>
 
         </div>
