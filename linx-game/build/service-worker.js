0cd095488c5a511d523ae1a4a85e397860bf8c02
Carlos Catarino <carlos_cata27@hotmail.com>
2020-05-28 21:15:46 -0500

    No se que sea pero lo subo

--- linx-game/build/service-worker.js
+++ linx-game/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.45ed9f68bdd9ec3122a258c7a95bbb53.js"
+  "/precache-manifest.2afd152de6ff896f1aba7e980a369e5c.js"
 );
 
 self.addEventListener('message', (event) => {
