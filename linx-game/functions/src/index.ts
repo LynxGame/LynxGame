422440916581396041b9772d6e8165fadd722031
Carlos Catarino <carlos_cata27@hotmail.com>
2020-05-30 21:09:53 -0500

    Update

--- linx-game/functions/src/index.ts
+++ linx-game/functions/src/index.ts
@@ -91,3 +91,148 @@ export const crearVideojuego = functions.https.onRequest(async(request,response)
         response.send(error)
     }
 })
+
+export const getAllGames = functions.https.onRequest(async(request,response)=>{
+    const connection = await connect();
+    const repoGame = connection.getRepository(Videojuegos);
+
+    const allGames = await repoGame.find();
+
+    response.send(allGames);
+})
+
+exports.getOneGame = functions.https.onRequest(async(request,response)=>{
+
+    const { Genero , nombre , descripcion , Desarrolladores, fecha, clasificion,Precio} = request.body;
+
+    try {
+        const connection = await connect();
+        const repoOneGame = connection.getRepository(Videojuegos);
+
+        const oneGame = await repoOneGame.createQueryBuilder("games")
+                                    .where("games.Genero = :Genero",{Genero : Genero})
+                                    .andWhere("games.nombre = :nombre",{nombre:nombre})
+                                    .andWhere("games.descripcion =:descripcion",{descripcion:descripcion})
+                                    .andWhere("games.Desarrolladores =:Desarrolladores",{Desarrolladores:Desarrolladores})
+                                    .andWhere("games.fecha = :fecha",{fecha:fecha})
+                                    .andWhere("games.clasificion =:clasificion",{clasificion:clasificion})
+                                    .andWhere("games.Precio=:Precio",{Precio:Precio})
+                                    .getOne();
+
+        response.send(oneGame);
+    } catch (error) {
+        response.send(error)
+    }
+})
+
+export const crearVenta = functions.https.onRequest(async(request,response)=>{
+
+    const { Cliente,fecha,VideoJuegos} = request.body;
+
+    try{
+        const connection=await connect();
+
+        const repoVenta = connection.getRepository(Venta);
+
+        const nuevoVenta = new Venta();
+        nuevoVenta.Cliente=Cliente;
+        nuevoVenta.VideoJuegos=VideoJuegos;
+        nuevoVenta.fecha=fecha;
+                
+        const guardarVenta = await repoVenta.save(nuevoVenta);
+
+        response.send(guardarVenta);
+    }catch(error){
+        response.send(error)
+    }
+})
+
+export const getAllVentas = functions.https.onRequest(async(request,response)=>{
+    const connection = await connect();
+    const repoVenta = connection.getRepository(Venta);
+
+    const allVenta = await repoVenta.find();
+
+    response.send(allVenta);
+})
+
+exports.getOneVenta = functions.https.onRequest(async(request,response)=>{
+
+    const { Cliente,fecha,VideoJuegos} = request.body;
+
+    try {
+        const connection = await connect();
+        const repoOneVenta = connection.getRepository(Venta);
+
+        const oneVenta = await repoOneVenta.createQueryBuilder("venta")
+                                    .where("venta.Cliente = :Cliente",{Cliente : Cliente})
+                                    .andWhere("venta.fecha = :fecha",{fecha:fecha})
+                                    .andWhere("venta.VideoJuegos =:Videojuegos",{VideoJuegos:VideoJuegos})
+                                    .getOne();
+
+        response.send(oneVenta);
+    } catch (error) {
+        response.send(error)
+    }
+})
+
+
+export const crearCliente = functions.https.onRequest(async(request,response)=>{
+
+    const { nombre,apellidos,username,email,edad,password,creditos,Tarjeta,calle,cp,numero,ciudad} = request.body;
+
+    try{
+        const connection=await connect();
+
+        const repoCliente = connection.getRepository(Cliente);
+
+        const nuevoCliente = new Cliente();
+        nuevoCliente.nombre=nombre;
+        nuevoCliente.apellidos=apellidos;
+        nuevoCliente.username=username;
+        nuevoCliente.email=email;
+        nuevoCliente.edad=edad;
+        nuevoCliente.password=password;
+        nuevoCliente.creditos=creditos;
+        nuevoCliente.Tarjeta=Tarjeta;
+        nuevoCliente.calle=calle;
+        nuevoCliente.cp=cp;
+        nuevoCliente.numero=numero;
+        nuevoCliente.ciudad=ciudad;
+                
+        const guardarCliente = await repoCliente.save(nuevoCliente);
+
+        response.send(guardarCliente);
+    }catch(error){
+        response.send(error)
+    }
+})
+
+export const getAllCliente = functions.https.onRequest(async(request,response)=>{
+    const connection = await connect();
+    const repoVenta = connection.getRepository(Cliente);
+
+    const allVenta = await repoVenta.find();
+
+    response.send(allVenta);
+})
+
+exports.getOneVenta = functions.https.onRequest(async(request,response)=>{
+
+    const { Cliente,fecha,VideoJuegos} = request.body;
+
+    try {
+        const connection = await connect();
+        const repoOneVenta = connection.getRepository(Venta);
+
+        const oneVenta = await repoOneVenta.createQueryBuilder("venta")
+                                    .where("venta.Cliente = :Cliente",{Cliente : Cliente})
+                                    .andWhere("venta.fecha = :fecha",{fecha:fecha})
+                                    .andWhere("venta.VideoJuegos =:Videojuegos",{VideoJuegos:VideoJuegos})
+                                    .getOne();
+
+        response.send(oneVenta);
+    } catch (error) {
+        response.send(error)
+    }
+})
