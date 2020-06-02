8456d0c81d95c82d244b17d1d8fa1c2cce954790
Miguel Angel <mikael.angelo.txt@gmail.com>
2020-06-02 03:23:07 -0500

    Pendiente...

--- linx-game/src/Vista/EditClient.js
+++ linx-game/src/Vista/EditClient.js
@@ -2,31 +2,37 @@ import React, { Component } from 'react'
 import { Table, Tag, Space } from 'antd';
 
 const { Column, ColumnGroup } = Table;
-var i=0;
-const data = [
-    {
-      Id_cliente: '1',
-      Nombre: 'Eduardo',
-      Apellido: 'Tuculo',
-      Username: 'Eduardin_12',
-      Email: 'eduardin_12@yahoo.com.mx',
-      Edad: 32,
-      Password: 'AngelAlberto:v',
-      Creditos: 0,
-      Calle: 'Tiro al Pichon',
-      CP: 2020,
-      Numero: 20,
-      Ciudad: 'Doofania',
-      banco: 'BBVA1',
-      numero: 4152639874512369,
-      fecha: '2020-05-29',
-      cvv: 123
-    }
-  ];
+var i=1;
 
+const columns = [
+    {
+      title: 'nombre',
+      dataIndex: 'nombre',
+      width: 150,
+    },
+    {
+      title: 'apellidos',
+      dataIndex: 'apellidos',
+      width: 150,
+    },
+    {
+      title: 'password',
+      dataIndex: 'password',
+    },
+  ];
+  
+  const data = [];
+  for (let i = 0; i < 3; i++) {
+    data.push({
+      id: i,
+      nombre: `Edward King ${i}`,
+      apellidos: 32,
+      password: `London, Park Lane no. ${i}`,
+    });
+  }
 
 class EditClient extends Component {
-
+/*
     state = {
         loading: false,
         cliente:{    
@@ -49,9 +55,9 @@ class EditClient extends Component {
             }
         }
     }
-
+*/
     async componentDidMount() {
-        const  url="https://us-central1-lynx-game.cloudfunctions.net/getAllGames";
+        const  url="https://us-central1-lynx-game.cloudfunctions.net/getAllCliente";
         const response = await fetch(url);
         const data = await response.json();
         this.setState({cliente: data[i], loading: false});
@@ -61,36 +67,7 @@ class EditClient extends Component {
   render() {
     return (
       <>
-         <Table dataSource={data} scroll={{ x: 1300 }} pagination={{ pageSize: 5 }}>
-    <Column title="Id_cliente" dataIndex="Id_cliente" key="Id_cliente" />
-    <ColumnGroup title="Name">
-      <Column title="Nombre" dataIndex="Nombre" key="Nombre" />
-      <Column title="Apellido" dataIndex="Apellido" key="Apellido" />
-    </ColumnGroup>
-    <Column title="Username" dataIndex="Username" key="Username"/>
-    <Column title="Email" dataIndex="Email" key="Email"/>
-    <Column title="Edad" dataIndex="Edad" key="Edad" />
-    <Column title="Password" dataIndex="Password" key="Password"/>
-    <Column title="Creditos" dataIndex="Creditos" key="Creditos"/>
-    <Column title="Calle" dataIndex="Calle" key="Calle"/>
-    <Column title="CP" dataIndex="CP" key="CP"/>
-    <Column title="Numero" dataIndex="Numero" key="Numero"/>
-    <Column title="Ciudad" dataIndex="Ciudad" key="Ciudad"/>
-    <Column title="banco" dataIndex="ID_tarjeta" key="ID_tarjeta"/>
-    <Column title="numero" dataIndex="Numero" key="Numero"/>
-    <Column title="fecha" dataIndex="Ciudad" key="Ciudad"/>
-    <Column title="cvv" dataIndex="ID_tarjeta" key="ID_tarjeta"/>
-    <Column
-      title="Action"
-      key="action"
-      render={(text, record) => (
-        <Space size="middle">
-          <a>Invite {record.lastName}</a>
-          <a>Delete</a>
-        </Space>
-      )}
-    />
-  </Table >
+        <Table columns={columns} dataSource={data} pagination={{ pageSize: 50 }} scroll={{ y: 240 }} />
       </>
     )
   }
