da9e8944beebd30c8be64870c210c005d69a5be6
Helix320 <64866893+Helix320@users.noreply.github.com>
2020-06-02 16:49:42 -0500

    enlace

--- linx-game/src/Vista/Admin.js
+++ linx-game/src/Vista/Admin.js
@@ -6,6 +6,7 @@ import Pie from './Pie';
 import Compra from './Compra';
 import EditGames from './EditGames';
 import EditClient from './EditClient';
+import Adminprofile from './Adminprofile';
 
 const { Header, Footer, Content } = Layout;
 
@@ -16,7 +17,7 @@ export class Admin extends Component {
                 <Header style={{padding:"0rem 0rem", position: 'sticky', zIndex:1,  top: "0" }}><ToolbarAdmin/></Header>
                     <Layout>
                         <Content style={{height:'auto', alignContent:'center'}}>
-                            <Route exact path="/" component={Compra} />
+                            <Route exact path="/Adminprofile" component={Adminprofile} />
                             <Route path="/Compra" component={Compra} />
                             <Route path="/EditGames" component={EditGames} />
                             <Route path="/EditClient" component={EditClient}/>
