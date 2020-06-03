3e3f353d344acfeb0c959f69d9ad910f872ec387
Juan Carlos <juancho131401@gmail.com>
2020-06-03 01:22:25 -0500

    Github Github

--- linx-game/src/Vista/App.js
+++ linx-game/src/Vista/App.js
@@ -12,7 +12,6 @@ import {ViewGame} from './ViewGame.js';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
  import {Carousel} from 'react-responsive-carousel'
  import ToolbarAdmin from './ToolbarAdmin';
- import ToolbarUser from './ToolbarUser';
 
 const { Header, Footer, Content } = Layout;
 
@@ -20,7 +19,7 @@ const { Header, Footer, Content } = Layout;
 function App() {
   return (
     <Layout>
-      <Header style={{padding:"0rem 0rem", position: 'sticky', zIndex:1,  top: "0" }}><ToolbarUser/></Header>
+      <Header style={{padding:"0rem 0rem", position: 'sticky', zIndex:1,  top: "0" }}><Toolbar/></Header>
       <Layout>
         <Content style={{height:'auto', alignContent:'center'}}>
             <Route exact path="/" component={Principal} />
@@ -28,7 +27,6 @@ function App() {
             <Route path="/Biblioteca" component={Biblioteca}/>
             <Route path="/Pay" component={Pay}/>
             <Route path="/ViewGame" component={ViewGame}/>
-           
         </Content>
       </Layout>
       <Footer>
